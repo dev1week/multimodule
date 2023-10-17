1d0cd209012d410dfeda07715a064e0cdc961bf7
dev1week <rondo2860@gmail.com>
2023-10-18 01:23:42 +0900

    feat: 스프링 빈 참조 확인 -성공(#1)
    컴포넌트 스캔이 이루어지는 package를 수정한다

--- module-api/src/main/java/dev/be/ModuleApiApplication.java
+++ module-api/src/main/java/dev/be/ModuleApiApplication.java
@@ -1,4 +1,5 @@
-package dev.be.moduleapi;
+//해당 패키지를 기준으로 컴포넌트 스캔을 한다.
+package dev.be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
+++ module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
@@ -1,3 +1,4 @@
+
 package dev.be.moduleapi.service;
 
 
