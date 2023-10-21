3ffb485de26d3fa338176f50541e5ac208797fe3
dev1week <rondo2860@gmail.com>
2023-10-21 14:50:01 +0900

    feat: local 환경 logback 설정(#16)

--- module-api/src/main/java/dev/be/moduleapi/controller/DemoController.java
+++ module-api/src/main/java/dev/be/moduleapi/controller/DemoController.java
@@ -4,13 +4,26 @@ package dev.be.moduleapi.controller;
 
 import dev.be.moduleapi.service.DemoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class DemoController {
+
+
+    @GetMapping("/log")
+    public String log(){
+      log.trace("trace");
+      log.debug("debug");
+      log.info("info");
+      log.warn("warn");
+      log.error("error");
+      return "demo";
+    }
 
     @Value("${profile-name}")
     private String name;
